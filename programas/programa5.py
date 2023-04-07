1afab4f088c490374573608589a13a4a444373b3
Unknown <facundo.diaz.rodriguez@fing.edu.uy>
2023-04-07 17:55:20 -0300

    Otra version del 5

--- programas/programa5.py
+++ programas/programa5.py
@@ -8,9 +8,20 @@ def prog(texto):
     aux=""
     num_pat=0
     num_responses=0
-    pat = re.findall(r'"patterns": (\[\s*.*?\s*\])', texto, flags=re.DOTALL)
-    res = re.findall(r'"responses": (\[\s*.*?\s*\])', texto, flags=re.DOTALL)
-    for i in pat:
+    pat = re.findall(r'"patterns": (\[\s*.*?\s*\])', texto, flags=re.DOTALL) # Otra solucion(sin utiliar DOTALL):("patterns": \[\s(\s*".*",*\s)*\s*\]) 
+    res = re.findall(r'"responses": (\[\s*.*?\s*\])', texto, flags=re.DOTALL) # ("responses": \[\s(\s*".*",*\s)*\s*\])
+    
+    tagResult = str(len(tag))
+
+    aux = "".join(pat)
+    patResult = str(len(re.findall(r'"(.*)"', aux)))
+    
+    aux = "".join(res)
+    respResult = str(len(re.findall(r'"(.*)"', aux)))
+
+    return tagResult + " " + patResult + " " + respResult
+
+    """ for i in pat:
         if it<len(pat)-1:
             patterns=re.split('",\n',i)
             num_pat=num_pat+len(patterns)
@@ -25,7 +36,7 @@ def prog(texto):
             num_responses=num_responses+len(responses)
             it=it+1
 
-    return str(it)+" "+str(num_pat)+" "+str(num_responses)
+    return str(it)+" "+str(num_pat)+" "+str(num_responses) """
     
 
 
