bfef8ffdb79639483edf88748d6a1753a474f484
Unknown <facundo.diaz.rodriguez@fing.edu.uy>
2023-04-17 01:34:12 -0300

    Ahora si arreglado

--- programas/programa4.py
+++ programas/programa4.py
@@ -3,24 +3,33 @@ import re
 import sys
 
 def prog(texto):
-    ''''tag = re.findall(r'"tag": "(.*)",', texto)
+    tag = re.findall(r'"tag": "(.*)",', texto)
     it=0
     result=""
-    #("tag": )"(.*)",\s*("patterns": \[\s+)".*",*(\s*".*",*)*\s*],\s*("responses": \[\s+)".*",*(\s*".*",*)*
-    #"tag"(.|\s)*?"context_set": ""
-    aux = re.findall(r'"tag"(.|\s)*?"context_set": ""', texto, flags=0)
+
+    #("tag": )"(.*)",\s*("patterns": \[\s+)".*",*(\s*".*",*)*\s*],\s*("responses": \[\s+)".*",*(\s*".*",*)* ->(V1)
+    #("tag": )"(.*)",\s*("patterns": \[((\s*".*",*)*)\s*],\s*)("responses": \[((\s*".*",*)*))\s*] ->(V2)
+    #"tag"(.|\s)*?"context_set": "" ->(V3)
+
+    #aux = re.finditer(r'("tag": )"(.*)",\s*("patterns": \[\s+)".*",*(\s*".*",*)*\s*],\s*("responses": \[\s+)".*",*(\s*".*",*)*', texto, flags=0)
+    #aux = re.finditer(r'"tag"(.|\s)*?"context_set": ""', texto, flags=0)
+    
+    aux = re.finditer(r'("tag": )"(.*)",\s*("patterns": \[((\s*".*",*)*)\s*],\s*)("responses": \[((\s*".*",*)*))\s*]', texto, flags=0)
     for i in aux:
+        pat = re.search(r'"patterns": \[((\s*".*",*)*)', i.group(0), flags=0)
+        pat = re.findall(r'\s*".*",*', pat.group(1), flags=0)
 
-        pat = re.findall(r'("patterns": \[\s+)".*",*(\s*".*",*)*', str(i), flags=0)
-        pat = re.findall(r'\s*".*",*', str(pat), flags=0)
+        res = re.search(r'"responses": \[((\s*".*",*)*)', i.group(0), flags=0)
+        res = re.findall(r'\s*".*",*', res.group(1), flags=0)
+        result = result + tag[it] + " " + str(len(pat)) + " " + str(len(res))
+        
+        if it<len(tag)-1:
+            result = result + "\n"
+        it+=1
 
-        res = re.findall(r'("responses": \[\s+)".*",*(\s*".*",*)*', str(i), flags=0)
-        res = re.findall(r'\s*".*",*', str(res), flags=0)
+    return result
 
-        result = result + tag[it] + " " + str(len(pat)) + " " + str(len(res)) + "\n"
-    return result''''
-
-    pat = re.findall(r'"patterns": (\[\s*.*?\s*\])', texto, flags=re.DOTALL)
+    """ pat = re.findall(r'"patterns": (\[\s*.*?\s*\])', texto, flags=re.DOTALL)
     res = re.findall(r'"responses": (\[\s*.*?\s*\])', texto, flags=re.DOTALL)
     for i in pat:
         if it<len(pat)-1:
@@ -36,9 +45,8 @@ def prog(texto):
             responses=re.split('",\n',res[it])
             num_responses=len(responses)
             aux=aux+tag[it]+" "+str(num_pat)+" "+str(num_responses)
-    return aux
+    return aux """
     
-
 
 if __name__ == '__main__':
     entrada = sys.argv[1]  # archivo entrada (param)
